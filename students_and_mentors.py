743c450ac509458e469d0e305e1b74d8a0ff584e
Alexander Radyuk <alexradyuk76@gmail.com>
2023-11-27 20:34:49 +0300

    Task number 2

--- students_and_mentors.py
+++ students_and_mentors.py
@@ -7,6 +7,15 @@ class Student:
         self.courses_in_progress = []
         self.grades = {}
 
+    def lecturer_skill(self, lecturer, course, grade):
+        if isinstance(lecturer, Lecturer) and course in lecturer.courses_attached and course in self.courses_in_progress:
+            if course in lecturer.grades:
+                lecturer.grades[course] += [grade]
+            else:
+                lecturer.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+
 
 class Mentor:
     def __init__(self, name, surname):
@@ -16,18 +25,46 @@ class Mentor:
 
 
 class Reviewer(Mentor):
-    pass
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+        self.courses_attached = []
+
+
+    def rate_hw(self, student, course, grade):
+        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
+            if course in student.grades:
+                student.grades[course] += [grade]
+            else:
+                student.grades[course] = [grade]
+        else:
+            return 'Ошибка'
 
 
 class Lecturer(Mentor):
-    pass
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+        self.grades = {}
+        self.courses_attached = []
 
 
 best_student = Student('Ruoy', 'Eman', 'your_gender')
-best_student.finished_courses += ['Git']
+best_student.finished_courses += ['Git', 'Python']
 best_student.courses_in_progress += ['Python']
 best_student.grades['Git'] = [10, 10, 10, 10, 10]
 best_student.grades['Python'] = [10, 10]
+
+some_reviewer = Reviewer('John', 'Checker')
+some_reviewer.courses_attached = ['Git']
+
+some_lecturer = Lecturer('Rob', 'Tutor')
+some_lecturer.courses_attached = ['Python']
+best_student.lecturer_skill(some_lecturer, 'Python', 10)
+print(some_lecturer.name, some_lecturer.surname)
+print(some_lecturer.name, some_lecturer.surname, 'has grade: ', some_lecturer.grades)
+print(some_lecturer.courses_attached)
+
+print(some_reviewer.name, some_reviewer.surname)
+print(some_reviewer.courses_attached)
 
 print(best_student.finished_courses)
 print(best_student.courses_in_progress)
